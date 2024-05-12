35a29905d4fca70d028abc613c0cf68ba545141b
RDM <rowand@gapp.nthu.edu.tw>
2024-05-12 14:09:52 +0800

    bruh2

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:fit_app/pages/home/home.dart';
 import 'package:fit_app/pages/home/home.dart';
 //ROWAN IS AWESOME1
 //to homepage commit 222 (new, alongside the updated userCreation Page)x
+//bruhh
 
 void main() {
   runApp(MainApp());
