df42b50a1839c5a85e6edcd3c8cb4c0da874988e
AngieP <angiepcnl@gmail.com>
2024-05-12 23:30:52 +0800

    sh

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:fit_app/pages/home/home.dart';
 //to homepage commit 222 (new, alongside the updated userCreation Page)x
 //bruhh kevin is awesome
 
-//angie was here
+//angie was heresssssss
 
 
 void main() {
