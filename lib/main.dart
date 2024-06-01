15546155d61e3a3a1a4a3fd8401c9078ef977c42
Jonathan Sutedjo 111006207 <jonathan.sutedjo@gapp.nthu.edu.tw>
2024-06-02 00:51:21 +0800

    Change Pp color theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:fit_app/pages/userCreation/pages/auth_page.dart';
 import 'package:fit_app/pages/userCreation/signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:fit_app/pages/home/home.dart';
-import 'package:fit_app/pages/home/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:fit_app/utilities/bottom_navigation_bar_height_provider.dart';
