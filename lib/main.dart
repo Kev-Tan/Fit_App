52dc3489b087135111a0bfedd866970c3e5087e8
Jonathan Sutedjo 111006207 <jonathan.sutedjo@gapp.nthu.edu.tw>
2024-06-02 01:15:57 +0800

    Edit a bit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MainApp extends StatelessWidget {
       create: (_) => BottomNavigationBarHeightProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: FormPage(),
+        home: Authpage(), //FormPage to open the Fill In Data page, Authpage to go to the main user page
       ),
     );
   }
--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -60,8 +60,8 @@ class _ProfilePageState extends State<ProfilePage> {
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.8),
-                spreadRadius: 2,
-                blurRadius: 6,
+                spreadRadius: 1,
+                blurRadius: 7,
                 offset: Offset(0, 5), // changes position of shadow
               ),
             ],
@@ -119,7 +119,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     color: Colors.grey.withOpacity(0.5),
                     spreadRadius: 5,
                     blurRadius: 7,
-                    //offset: Offset(0, 3), // changes position of shadow
+                    offset: Offset(0, 80), // changes position of shadow
                   ),
                 ],
               ),
