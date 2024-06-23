ac38f5c7a2aca26a27a2c919661fc911f807189e
Jonathan Sutedjo 111006207 <jonathan.sutedjo@gapp.nthu.edu.tw>
2024-06-23 19:39:50 +0800

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:fit_app/pages/userCreation/forgot_password_page.dart';
 import 'package:fit_app/pages/userCreation/login_page.dart';
-import 'package:fit_app/pages/userCreation/pages/auth_page.dart';
 import 'package:fit_app/pages/userCreation/pages/form.dart';
 import 'package:fit_app/pages/userCreation/signup_page.dart';
-import 'package:flutter/material.dart';
 import 'package:fit_app/pages/home/home.dart';
+import 'package:fit_app/pages/userCreation/pages/auth_page.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:fit_app/utilities/bottom_navigation_bar_height_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
+  tz.initializeTimeZones(); //to initialize the timezones we are using 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
