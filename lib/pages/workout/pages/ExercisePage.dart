ff34f91806d7c4a771424ed542b2e74cbe21fabf
angelicacheng <angelicacheng@gapp.nthu.edu.tw>
2024-06-25 05:05:46 +0800

    more edits

--- lib/pages/workout/pages/ExercisePage.dart
+++ lib/pages/workout/pages/ExercisePage.dart
@@ -27,13 +27,29 @@ class _ExercisePageState extends State<ExercisePage> {
     }
   }
 
+  String _capitalizeFirstLetterOfEachWord(String input) {
+    return input
+        .split(' ')
+        .map((word) => word[0].toUpperCase() + word.substring(1))
+        .join(' ');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fetch Exercise'),
+        title: Text(
+          'Fetch Exercise',
+          style: TextStyle(
+            fontSize: 24.0,
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.background,
       ),
-      body: Padding(
+      body: Container(
+        color: Theme.of(context).colorScheme.background,
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
@@ -41,25 +57,48 @@ class _ExercisePageState extends State<ExercisePage> {
               controller: _controller,
               decoration: InputDecoration(
                 labelText: 'Enter Exercise Name',
+                labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
               ),
+              style: TextStyle(color: Theme.of(context).colorScheme.primary),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 40),
             ElevatedButton(
               onPressed: () {
                 _fetchExerciseInfo(_controller.text);
               },
-              child: Text('Fetch Exercise Info'),
+              child: Text(
+                'Fetch Exercise Info',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.background,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                padding: EdgeInsets.all(20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 40),
             _exercises.isEmpty
-                ? Text('No exercise data found')
+                ? Text(
+                    'No exercise data found',
+                    style: TextStyle(color: Theme.of(context).colorScheme.primary),
+                  )
                 : Expanded(
                     child: ListView.builder(
                       itemCount: _exercises.length,
                       itemBuilder: (context, index) {
                         final exercise = _exercises[index];
                         if (exercise.containsKey('error')) {
-                          return Text(exercise['error'] ?? 'Error');
+                          return Text(
+                            exercise['error'] ?? 'Error',
+                            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+                          );
                         }
                         return ExerciseCard(exercise: exercise);
                       },
@@ -126,25 +165,35 @@ class ExerciseCard extends StatelessWidget {
 
   ExerciseCard({required this.exercise});
 
+  String _capitalizeFirstLetterOfEachWord(String input) {
+    return input
+        .split(' ')
+        .map((word) => word[0].toUpperCase() + word.substring(1))
+        .join(' ');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(vertical: 10),
+      color: Theme.of(context).colorScheme.background,
+      elevation: 10,
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              exercise['name'] ?? '',
+              _capitalizeFirstLetterOfEachWord(exercise['name'] ?? ''),
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary,
               ),
             ),
             SizedBox(height: 10),
-            Text('Body Part: ${exercise['bodyPart'] ?? ''}'),
-            Text('Target: ${exercise['target'] ?? ''}'),
+            Text('Body Part: ${exercise['bodyPart'] ?? ''}', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
+            Text('Target: ${exercise['target'] ?? ''}', style: TextStyle(color: Theme.of(context).colorScheme.primary)),
             SizedBox(height: 10),
             exercise['gifUrl'] != null && exercise['gifUrl'].isNotEmpty
                 ? Image.network(exercise['gifUrl'])
@@ -154,10 +203,11 @@ class ExerciseCard extends StatelessWidget {
               'Instructions:',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary,
               ),
             ),
             ...?exercise['instructions']?.map(
-              (instruction) => Text(instruction),
+              (instruction) => Text(instruction, style: TextStyle(color: Theme.of(context).colorScheme.primary)),
             ),
           ],
         ),
