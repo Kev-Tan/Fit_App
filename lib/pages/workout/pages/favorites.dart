d1639612c9f5c74786fe94f62f6cc0d3a814e744
KevinT <kevinsutan99@gmail.com>
2024-06-24 17:06:23 +0800

    favorties fully implemented

--- lib/pages/workout/pages/favorites.dart
+++ lib/pages/workout/pages/favorites.dart
@@ -10,6 +10,41 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
+  void _removeFromFavorites(String exerciseName) async {
+    setState(() {
+      widget.userProvider.user!.favorites!.remove(exerciseName);
+    });
+
+    UserModel updatedUser = UserModel(
+      uid: widget.userProvider.user!.uid,
+      username: widget.userProvider.user!.username,
+      profileImageUrl: widget.userProvider.user!.profileImageUrl,
+      email: widget.userProvider.user!.email,
+      gender: widget.userProvider.user!.gender,
+      age: widget.userProvider.user!.age,
+      height: widget.userProvider.user!.height,
+      weight: widget.userProvider.user!.weight,
+      neck: widget.userProvider.user!.neck,
+      waist: widget.userProvider.user!.waist,
+      hips: widget.userProvider.user!.hips,
+      goal: widget.userProvider.user!.goal,
+      level: widget.userProvider.user!.level,
+      frequency: widget.userProvider.user!.frequency,
+      duration: widget.userProvider.user!.duration,
+      time: widget.userProvider.user!.time,
+      favorites: widget.userProvider.user!.favorites,
+    );
+
+    await widget.userProvider.updateUser(updatedUser);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Exercise removed from favorites!'),
+        duration: Duration(seconds: 3),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = widget.userProvider.user!;
@@ -25,6 +60,12 @@ class _FavoritesPageState extends State<FavoritesPage> {
               itemBuilder: (context, index) {
                 return ListTile(
                   title: Text(favorites[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () {
+                      _removeFromFavorites(favorites[index]);
+                    },
+                  ),
                 );
               },
             )
